ec63a59ccc1d60a9247f2d011bd1496062ca1a32
chapin11 <seedyou36@gmail.com>
2025-06-22 18:23:04 +0300

    fix bags
    fix bags with ymaps version

--- src/components/YandexMap.tsx
+++ src/components/YandexMap.tsx
@@ -1,4 +1,4 @@
-import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
+import { YMaps, Map, Placemark } from "@iminside/react-yandex-maps";
 
 export default function YandexMap() {
   return (
